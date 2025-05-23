9f1894fa0efd98a3f2fab39bf2fe5c4a01ccf850
pesppor676 <pedroespagna78@gmail.com>
2025-05-23 18:42:27 +0200

    primer commit

---
+++ practica-espana.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @description Funcion para obtener los detalles de un usuario
+ * @file practica-espana.php
+ * @param PDO $pdo Conexi�n a la base de datos
+ * @param int $id id del usuario
+ * @return array|bool array con los detalles del usuario o false si hay error
+ * @author Pedro
+ * @date 2025/05/11
+ * @version 1.0
+ * CREAMOS ESTE COMENTARIO PARA HACER UN COMMIT
+ */
+function detallesUsuario(PDO $pdo, int $id) : array|bool
+{
+    $sql = "SELECT * FROM usuarios WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+    $resultado = false;
+    try {
+        if ($stmt->execute()) {
+            $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+    } catch (PDOException $e) {
+        $resultado = false;
+    }
+    return $resultado;
+}
+
+
+   /**
+ * @description Funcion para obtener los empleados que son coordinadores
+ * @file practica-espana.php
+ * @param PDO $pdo Conexi�n a la base de datos
+ * @return array|bool array con los empleados o false si hay error
+ * @author Pedro
+ * @date 2025/05/11
+ * @version 1.0
+ */
+function listadoCoordinadores(PDO $pdo) : array|bool
+{
+    $sql = <<<ENDSQL
+        SELECT * FROM empleados WHERE 
+            find_in_set('coord',roles)>0 or
+            find_in_set('trasoc',roles)>0
+    ENDSQL;
+    $stmt = $pdo->prepare($sql);
+    try {
+        $stmt->execute();
+        $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    } catch (PDOException $e) {
+        $resultado = false;
+    }
+    return $resultado;
+}
+function detallesUsuario(PDO $pdo, int $id) : array|bool
+{
+    $sql = "SELECT * FROM usuarios WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+    $resultado = false;
+    try {
+        if ($stmt->execute()) {
+            $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+    } catch (PDOException $e) {
+        $resultado = false;
+    }
+    return $resultado;
+}
+
+    ?>
